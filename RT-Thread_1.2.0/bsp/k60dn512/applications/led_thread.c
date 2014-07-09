af7a553cce1b8eb869fa68bdcacc6bea1cef5af9
liubins313 <liubins313@126.com>
2014-07-09 18:20:28 +0800

    将文件组织成RT-thread的标准形式

---
+++ RT-Thread_1.2.0/bsp/k60dn512/applications/led_thread.c
@@ -0,0 +1,18 @@
+#include <rtthread.h>
+#include <rthw.h>
+
+#include "gpio.h"
+
+void led_thread_entry(void* parameter)
+{
+    GPIO_QuickInit(HW_GPIOE, 6, kGPIO_Mode_OPP);
+	while(1)
+	{
+//        GUI_Exec();
+  //      GUI_TOUCH_Exec();
+        GPIO_ToggleBit(HW_GPIOE, 6);
+        rt_thread_delay(10);
+	}
+}
+
+
